8ce04e8c5a48199b12aec21eb5dccaa5a99e14b1
Lisa <savushkina.lisa.00027@gmail.com>
2020-01-12 17:13:34 +0300

    Main Files from Unity
    Base Files

---
+++ Assets/Scripts/Camera_Control.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pers;
+    public float smooth;
+    Vector3 pers_pos;
+    Camera cam;
+    void Start()
+    {
+        pers_pos = Vector3.zero;
+        pers_pos.z = -10;
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+    }
+
+    void CameraFollow()
+    {
+        pers_pos = pers.transform.position;
+        pers_pos.z = -10;
+        cam.transform.position = pers_pos * smooth;
+    }
+    void CameraPositionTransform(Vector3 new_pos)
+    {
+        new_pos.z = -10;
+        cam.transform.position = new_pos;
+    }
+}
---
+++ Assets/Scripts/Move_Control.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed; // speed of our moves
+    Vector3 change; // changes per click
+    Rigidbody2D pers;
+    Animator anim;
+    void Start()
+    {
+        change = new Vector3(0, 0, 0);
+        pers = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change.x = Input.GetAxisRaw("Horizontal"); // read of "A" - "D" and "Left" - "Right" buttons
+        change.y = Input.GetAxisRaw("Vertical"); // read of "W" - "S" and "Up" - "Down" buttons
+
+        if (change != Vector3.zero) // transform position
+        {
+            MoveCharacter();
+            AnimationCharacter(change.x, change.y);
+        }
+        else
+        {
+            anim.SetBool("move", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        //pers.MovePosition(transform.position + change * speed * Time.deltaTime);
+        transform.position += change * speed;
+    }
+    void AnimationCharacter(float x, float y)
+    {
+        anim.SetFloat("x", x);
+        anim.SetFloat("y", y);
+        anim.SetBool("move", true);
+
+
+    }
+}
